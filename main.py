1c4774e68a7885af5b203347021b9c88a791d10d
taijitsuhitsyou <hy.nguyenduc452@gmail.com>
2024-05-08 21:03:28 +1000

    added class: character with stats

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+class Character:
+    def __init__(self, name, strength, dexterity, constitution, intelligence, wisdom, charisma):
+        self.name = name
+        self.strength = strength
+        self.dexterity = dexterity
+        self.constitution = constitution
+        self.intelligence = intelligence
+        self.wisdom = wisdom
+        self.charisma = charisma
+    
