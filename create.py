981c0771616144a09829f867c126c929567e4fec
taijitsuhitsyou <hy.nguyenduc452@gmail.com>
2024-05-10 02:04:44 +1000

    added rest of the stats

--- create.py
+++ create.py
@@ -1,20 +1,21 @@
 import os, json, random
 
 class Character:
-    def __init__(self, name, race, strength, dexterity, constitution):
+    def __init__(self, name, race, strength, dexterity, constitution, intelligence, wisdom, charisma):
         self.name = name
         self.race = race
         self.strength = strength
         self.dexterity = dexterity
         self.constitution = constitution
+        self.intelligence = intelligence
+        self.wisdom = wisdom
+        self.charisma = charisma
 
 class Character_choice(Character):
-    def __init__(self, name, race, strength, dexterity, constitution, select_choice): #, intelligence, wisdom, charisma):
-        super().__init__(name, race, strength, dexterity, constitution)
+    def __init__(self, name, race, strength, dexterity, constitution, intelligence, wisdom, charisma, select_choice): #):
+        super().__init__(name, race, strength, dexterity, constitution, intelligence, wisdom, charisma)
         self.select_choice = select_choice
-        # self.intelligence = intelligence
-        # self.wisdom = wisdom
-        # self.charisma = charisma
+
     
     def roll_dice(self):
         # Roll a 20-sided dice (D20) and add the character's relevant ability modifier
@@ -30,7 +31,7 @@ class Character_choice(Character):
             1: -5, 2: -4, 3: -4, 4: -3, 5: -3, 6: -2, 7: -2, 8: -1, 9: -1,
             10: 0, 11: 0, 12: +1, 13: +1, 14: +2, 15: +2, 16: +3, 17: +3, 18: +4, 19: +4, 20: +5
         }
-        stats = [(3, self.strength), (4, self.dexterity), (5, self.constitution)]
+        stats = [(3, self.strength), (4, self.dexterity), (5, self.constitution), (6, self.intelligence), (7, self.wisdom), (8, self.charisma)]
         for stat in range(len(stats)):
             if stat+3 == self.select_choice:
                 modifier = modifiers[stats[stat][1]] 
@@ -44,7 +45,10 @@ def create_character():
     strength = int(input("Enter character strength: "))
     dexterity = int(input("Enter character dexterity: "))
     constitution = int(input("Enter character constitution: "))
-    return Character(name, race, strength, dexterity, constitution)
+    intelligence = int(input("Enter character intelligence: "))
+    wisdom = int(input("Enter character wisdom: "))
+    charisma = int(input("Enter character charisma: "))
+    return Character(name, race, strength, dexterity, constitution, intelligence, wisdom, charisma)
 
 def save_to_json(character, filename):
     with open(filename, 'w') as file:
